cd735dc88a8d013a23810eeb2369e976cb7dcea1
akshay-dev-29 <akshayladekar07@gmail.com>
2024-12-07 13:06:58 +0530

    added scripts in the package.json to test and added refactored code in stringCalculator.js

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,22 @@
-function add(numbers){
+function add(numbers) {
+    if (numbers === "") return 0;
 
+    let delimiter = /,|\n/;
+
+    if (numbers.startsWith("//")) {
+        const match = numbers.match(/^\/\/(.+)\n/);
+        if (match) {
+            delimiter = new RegExp(match[1]);
+            numbers = numbers.slice(match[0].length)
+        }
+    }
+
+    const numArray = numbers.split(delimiter).map(Number)
+    const negatives = numArray.filter((num) => num < 0)
+    if (negatives.length > 0) {
+        throw new Error(`negative numbers are not allowed: ${negatives.join(", ")}`)
+    }
+    return numArray.reduce((sum, num) => sum + num, 0)
 }
 
 module.exports = { add }
